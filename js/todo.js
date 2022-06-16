d756a7f226886308189a7b9087bc1298a046fcbc
zahriddinulmasov <zahriddinus@gmail.com>
2022-06-16 13:14:50 +0500

    init: to correct

--- js/todo.js
+++ js/todo.js
@@ -1,39 +1,5 @@
 "use strick";
 
-// const elForm = document.querySelector(".form");
-// const elInput = document.querySelector(".input");
-// const elList = document.querySelector(".list");
-
-// const todos = [];
-// // let counter = 0;
-
-// elForm.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
-
-//   const userValue = elInput.value;
-
-//   const todo = {
-//     name: userValue,
-//     id: todos.length,
-//   };
-
-//   todos.push(todo);
-
-//   console.log(todos);
-
-//   elInput.value = null;
-
-//   elList.innerHTML = null;
-
-//   for (let item of todos) {
-//     const newLi = document.createElement("li");
-
-//     newLi.textContent = item.name;
-
-//     elList.appendChild(newLi);
-//   }
-// });
-
 const elForm = document.querySelector(".form");
 const elFormName = document.querySelector(".form__name");
 const elFormOldPrice = document.querySelector(".form__old-price");
